c3e45e0d0c096a7e20c21fe016431dd272796021
hackgosu <sacoredev@gmail.com>
2022-03-11 14:57:18 +0900

    Initial Commit

---
+++ Objective-C_Practice/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Objective-C_Practice
+//
+//  Created by 박성수 on 2022/03/11.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
